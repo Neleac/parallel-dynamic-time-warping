667a0075478892e02191a7d7d6653983b8673a2d
Caelen <caelenmensch@gmail.com>
2022-12-07 20:28:11 -0800

    parallelize across diagonals

---
+++ parallel.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <omp.h>
+
+using namespace std;
+
+int costFunction(int v1, int v2) {
+    return abs(v1 - v2);
+}
+
+int dtw(int** DP, int* s, int* t, int n, int m) {
+    for (int diag = 0; diag < n + m - 1; diag++) {
+        int i = min(diag, n - 1);
+        int j = max(0, diag - n + 1);
+        int count = min(i + 1, m - j);
+        
+        #pragma omp parallel for
+        for (int idx = 0; idx < count; idx++) {
+            int row = i - idx + 1;
+            int col = j + idx + 1;
+
+            int cost = costFunction(s[row - 1], t[col - 1]);
+            DP[row][col] = cost + min({DP[row - 1][col], DP[row][col - 1], DP[row - 1][col - 1]});
+        }
+    }
+    return DP[n][m];
+}
